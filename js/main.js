19f409af9de42f7496a293468e352a8998251252
YumMinSeok <minseok@sangilmedia.hs.kr>
2024-08-18 13:03:39 +0900

    First commit
    성빈이가 한거 깃에다 올림

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// main.js
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.new-logo-container').classList.add('move-logo');
+        document.querySelector('.text').classList.add('move-text');
+        document.querySelector('.text2').classList.add('move-text2');
+        document.querySelector('.start-button-container').classList.add('move-start-button');
+    } ,0);
+});
---
+++ js/signup.js
@@ -0,0 +1,23 @@
+document.getElementById('phone').addEventListener('input', function (e) {
+    let value = e.target.value.replace(/[^0-9]/g, '');
+    if (value.length > 3 && value.length <= 7) {
+        value = value.replace(/(\d{3})(\d{1,4})/, '$1-$2');
+    } else if (value.length > 7) {
+        value = value.replace(/(\d{3})(\d{4})(\d{1,4})/, '$1-$2-$3');
+    }
+    e.target.value = value;
+});
+
+function validateForm() {
+    const email = document.getElementById('email').value;
+    const emailError = document.getElementById('email-error');
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailPattern.test(email)) {
+        emailError.style.display = 'block';
+        return false;
+    } else {
+        emailError.style.display = 'none';
+    }
+    return true;
+}
